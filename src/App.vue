9f76b64bf06cc5b6e01d80a552e7646151b30903
KonradB <konradbukowicz@macbook-pro-konrad.home>
2021-05-29 11:17:00 +0200

    Added login and buttons for login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <h1>Twoj email to: {{ email }}</h1> -->
+    <h1>Sprawdzanie adresu email</h1>
+    <h2>Twoj email to: {{ email }}</h2>
+
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
+      <!-- BINDING, reaktywnosc - Aplikacja reaktywnie zmienia stan komponentu i we wszystkich miejscach gdzie ten komponent zostal uzyty, ta zmiana sie aktualizuje -->
+    <input type= "email" v-model="email">  <!-- jak robimy odwolnie do v-modelu, to jesli cos zmieniamy w oknie do wypisywania ro wniez sie to zmienia w naszym tekscie wyjsciowym  -->
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+  
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      email: 'bukowicz@student.agh.edu.pl',
+      password: ''
+    };
+  },
+
+  methods: {
+  alertMyEmail() {
+    alert(this.email);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
