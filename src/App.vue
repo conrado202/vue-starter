77983789c1efc7662eaea9cb949d30cc973f77e0
KonradB <konradbukowicz@macbook-pro-konrad.home>
2021-05-29 11:45:38 +0200

    Installed milligram and updated page for login

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    <!-- <h1>Twoj email to: {{ email }}</h1> -->
+    <!-- CZESC 1 -->
+    <!--<h1>Twoj email to: {{ email }}</h1>
     <h1>Sprawdzanie adresu email</h1>
     <h2>Twoj email to: {{ email }}</h2>
 
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div> -->
 
-      <!-- BINDING, reaktywnosc - Aplikacja reaktywnie zmienia stan komponentu i we wszystkich miejscach gdzie ten komponent zostal uzyty, ta zmiana sie aktualizuje -->
-    <input type= "email" v-model="email">  <!-- jak robimy odwolnie do v-modelu, to jesli cos zmieniamy w oknie do wypisywania ro wniez sie to zmienia w naszym tekscie wyjsciowym  -->
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
-  
+    <!-- BINDING, reaktywnosc - Aplikacja reaktywnie zmienia stan komponentu i we wszystkich miejscach gdzie ten komponent zostal uzyty, ta zmiana sie aktualizuje -->
+    <!-- jak robimy odwolnie do v-modelu, to jesli cos zmieniamy w oknie do wypisywania ro wniez sie to zmienia w naszym tekscie wyjsciowym  -->
+    <!--<input type= "email" v-model="email">  
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button> -->
+
+    <!-- CZESC 2 -->
+    <h1>Witaj w systemie do zapisow na zajecia</h1>
+      <div v-if="authenticated">
+         <h3>Jestes zalogowany jako: {{email}}</h3>
+         <button @click="logOut()">Wyloguj sie</button>
+      </div>
+
+      <div v-else>
+         Zaloguj sie:
+              <input type= "text" v-model="email">  
+              <button @click="logIn()">Zaloguj sie</button>
+      </div>
+
   </div>
 
 
@@ -21,22 +36,54 @@
 
 
 export default {
+// CZESC 1
+  // data() {
+  //   return {
+  //     email: 'bukowicz@student.agh.edu.pl',
+  //     password: ''
+  //   };
+  // },
+
+  // methods: {
+  // alertMyEmail() {
+  //   alert(this.email);
+  //   }
+  // }
+
+// CZESC 2
   data() {
     return {
-      email: 'bukowicz@student.agh.edu.pl',
-      password: ''
+      authenticated: false
     };
   },
 
   methods: {
-  alertMyEmail() {
-    alert(this.email);
+    logIn() {
+      this.authenticated = true;
+    },
+    logOut() {
+      this.authenticated = false;
+      this.email = '';
     }
   }
+
 }
+</script>
+
+<script>
+  import "milligram";
+
+  export default {
+    // ...
+  };
 </script>
 
 <style>
 
+  /* Wszystkie znaczniki z odnosniekiem "button" maja miec zadany kolor */
+button {
+  background: blue;
+  color: white;
+}
 
 </style>
